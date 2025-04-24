d60542c08464dfa51b627c5bd9947a3c95a623f6
Legolas1148 <niteeshrawal1@gmail.com>
2025-04-24 14:38:20 -0400

    Added win message for nonogram

--- routes/nonogram/components/gameBoard.jsx
+++ routes/nonogram/components/gameBoard.jsx
@@ -47,6 +47,7 @@ function renderGameBoard({ gameState, setGameState }, gameController) {
           ))}
         </div>
       ))}
+      {gameState.gameWon}?<h3>Congratulations! You Won</h3>:;
     </>
   );
 }
